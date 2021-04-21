2157ee1fa15f0e436d5d5d7e559474bf8c10f495
Marco Barbosa <marco.barbosa@base2.com.br>
2021-04-21 19:41:53 -0300

    servidor cors pagseguro checkout

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { urlencoded } = require('body-parser');
+const jsonToUrlEncoded = require("form-urlencoded");
+const axios = require('axios');
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.send('maxShop!!!'));
+
+app.post('/', async (req, res) => {
+    // recebe os dados passados
+    const body = req.body;
+    let response;
+
+    // API
+    const apiToFetch = "https://ws.sandbox.pagseguro.uol.com.br/v2/checkout?email=marco_meireles@ig.com.br&token=AA91EBBCCCD547E9ABA425583D6B59BA";
+
+    // cria a string a ser passada
+    const payload = jsonToUrlEncoded(body);
+    
+    // make request to IEX API and forward response
+    await axios(apiToFetch, {
+      method: 'post',
+      data: payload
+    })
+    .then((res) => {
+      //console.log(`statusCode: ${res.statusCode}`);
+      //console.log(res.data);
+      if(res){
+        response = res.data;
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+    
+    // Mandando a resposta para fora
+    res.send(response);
+});
+app.listen(port, () => console.log(`http://localhost:${port}`));
